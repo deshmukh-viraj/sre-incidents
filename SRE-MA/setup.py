a1cd9ef2a7626f46bd5974139a3ef46fc2070abc
Viraj Deshmukh <virajdeshmukh080818@gmail.com>
2026-07-01 15:06:47 +0530

    chore: updated

--- SRE-MA/setup.py
+++ SRE-MA/setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='A short description of the project.',
+    description='Autonomous SRE Multi-Agent System for Real-Time Incident Detection, Diagnosis, and Remediation.',
     author='VIRAJ DESHMUKH',
     license='MIT',
 )
